e432e5049c8e9007b5ba4b85c6aee49d3b9a7e62
Adewoyin, David <David.Adewoyin@viacom.com>
2017-12-17 21:47:11 -0500

    added metronome pro assets, some sounds, & little scripting on drumaudiocontrols

--- Assets/Scripts/Controls/DrumAudio.cs
+++ Assets/Scripts/Controls/DrumAudio.cs
@@ -2,25 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DrumAudioControls : MonoBehaviour {
+public class DrumAudio : MonoBehaviour {
 
 	AudioSource m_MyAudioSource;
 
-	//Play the music
-	private bool m_Play;
-	//Detect when you use the toggle, ensures music isn’t played multiple times
-	private bool m_ToggleChange;
-
 	// Use this for initialization
 	void Start () {
+		
 		//Fetch the AudioSource from the GameObject
 		m_MyAudioSource = GetComponent<AudioSource>();
-		//Ensure the toggle is set to true for the music to play at start-up
-		m_Play = true;
+
 	}
-	
+
+	void OnMouseDown() {
+		Debug.Log ("Testing");
+		m_MyAudioSource.Play ();
+		Debug.Log (m_MyAudioSource);
+	}	
+
 	// Update is called once per frame
 	void Update () {
-		
+//		Debug.Log("Update");
 	}
+
 }
---
+++ Assets/Scripts/Controls/PlayRecord.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRecord : MonoBehaviour {
+
+	private bool isRecording;
+	private float timeCounter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+	public void onStartRecording() {
+		timeCounter = 0f;
+		isRecording = true;
+		Debug.Log ("Play button");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isRecording) {
+			timeCounter++;
+			Debug.Log ("We live baby");
+		}
+	}
+}
